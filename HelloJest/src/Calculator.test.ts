4ea3c3fe3a0cbc26216ee9dcc1a8f22f63bd3576
ChiragRupani <36813124+chiragrupani@users.noreply.github.com>
2018-07-08 20:29:07 +0530

    Removed Sample test file in src

--- HelloJest/src/Calculator.test.ts
+++
@@ -1,8 +0,0 @@
-import calculate from '../src/Calculator';
-
-describe('calculate', function() {
-  it('add', function() {
-    let result = calculate(5, 2);
-    expect(result).toBe(7);
-  });
-});

37d380a323c9f07b73c0c83b91815c27c0ddd73e
Chirag Rupani <36813124+chiragrupani@users.noreply.github.com>
2023-07-07 09:23:53 +0530

    Added coverage config

--- HelloVitest/vite.config.ts
+++ HelloVitest/vite.config.ts
@@ -4,5 +4,8 @@ import { defineConfig } from "vitest/config";
 export default defineConfig({
   test: {
     globals: true,
+    coverage: {
+      reporter: [["cobertura", { file: "coverage.xml" }], "text"],
+    },
   },
 });

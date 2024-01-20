a4c4041218752bfd0cf66c9bbea26fa1d69ead92
Chirag Rupani <36813124+ChiragRupani@users.noreply.github.com>
2024-01-20 13:53:47 +0530

    Updated dependencies

--- HelloMocha/tests/Calculator.test.ts
+++ HelloMocha/tests/Calculator.test.ts
@@ -1,14 +1,14 @@
-import { expect } from 'chai';
-import C from '../src/Calculator';
+import { expect } from "chai";
+import calculator from "../src/Calculator";
 
-describe('calculate', function() {
-  it('add', function() {
-    let result = C.Sum(5, 2);
+describe("calculate", function () {
+  it("add", function () {
+    let result = calculator.Sum(5, 2);
     expect(result).equal(7);
   });
 
-  it('substract', function() {
-    let result = C.Difference(5, 2);
+  it("substract", function () {
+    let result = calculator.Difference(5, 2);
     expect(result).equal(3);
   });
 });
